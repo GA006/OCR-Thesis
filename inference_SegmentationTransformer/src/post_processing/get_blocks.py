db62b74b98ef5e0aa67335bfa72c8360a17dfe74
GA006 <gogo.angelchev006@gmail.com>
2022-06-02 12:51:00 +0300

    SETR inference

---
+++ inference_SegmentationTransformer/src/post_processing/get_blocks.py
@@ -0,0 +1,74 @@
+import os
+import cv2
+import numpy as np
+from matplotlib import image
+from PIL        import ImageDraw
+import matplotlib.pyplot as plt
+
+
+import src.ocr.tesseract_agent as tes
+from src.elements import Rectangle, TextBlock, Layout
+
+
+#results is 2d prediction, classes is label map
+def bbox_yield(result, label_map):
+    layout = Layout()
+
+    for i in range(1,len(list(label_map))):
+        arr = result != i
+
+        plt.imsave('./filename.png', arr, cmap='binary')
+        img = cv2.imread('./filename.png')
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        thresh = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY)[1]
+        contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours = contours[0] if len(contours) == 2 else contours[1]
+        
+        for cntr in contours:
+            x, y, w, h = cv2.boundingRect(cntr)
+            layout.append(TextBlock(Rectangle(x,y,x+w,y+h), type=label_map[i]))
+        
+        os.remove('./filename.png')
+    
+    return layout
+
+def class_of_interest(layout, roi):
+    text_blocks  = Layout([b for b in layout if b.type==roi])
+    other_blocks = Layout([b for b in layout if b.type!=roi])
+    return Layout([b for b in text_blocks if not any(b.is_in(b_fig) for b_fig in other_blocks)]), other_blocks
+
+def remove_empty_rectangles(image, layout, threshold=50):
+    ocr_agent = tes.TesseractAgent(languages='eng') 
+
+    arr_im = np.asarray(image)
+    new_layout = Layout()
+
+    for block in layout:
+        segment_image = (block.crop_image(arr_im))
+
+        text = ocr_agent.detect(segment_image)
+        if len(text) >= threshold: 
+            new_layout.append(block)
+    
+    return new_layout
+
+def mask_other_regions(image, blocks):
+    draw = ImageDraw.Draw(image)
+    for i in blocks:
+        draw.rectangle([i.block.x_1, i.block.y_1, i.block.x_2, i.block.y_2], fill=(255,255,255))
+    return image
+
+def ocr(image, layout):
+    ocr_agent = tes.TesseractAgent(languages='eng')
+
+    arr_im = np.asarray(image)
+    for block in layout:
+        segment_image = (block.crop_image(arr_im))
+
+        text = ocr_agent.detect(segment_image)
+        block.set(text=text, inplace=True)
+
+    for txt in layout.get_texts():
+        print(txt, end='\n---\n')
+
+    return layout
---
+++ inference_SegmentationTransformer/src/post_processing/post_segmentation.py
@@ -0,0 +1,129 @@
+import numpy as np
+from PIL import ImageDraw
+
+import src.elements.layout as lay
+import src.elements.layout_elements as le
+
+
+class PostProcessingSegmentation():
+
+    def __init__(self, blocks, image):
+        self.blocks    = blocks
+        self.image     = image
+
+    def intersection_over_union(self, a, b):
+        x1 = max(a.block.x_1, b.block.x_1)
+        y1 = max(a.block.y_1, b.block.y_1)
+        x2 = min(a.block.x_2, b.block.x_2)
+        y2 = min(a.block.y_2, b.block.y_2)
+        if (y2-y1) <= 0 or (x2-x1) <= 0:
+            return 0
+
+        intersection = (x2-x1)*(y2-y1)
+
+        areaA = (a.block.x_2 - a.block.x_1) * (a.block.y_2 - a.block.y_1)
+        areaB = (b.block.x_2 - b.block.x_1) * (b.block.y_2 - b.block.y_1)
+
+        iou = intersection / (areaA + areaB - intersection)
+
+        return iou
+
+    def unions(self, threshold=500):
+        unions = []
+        disappear = []
+        count = 1
+        bl = self.blocks
+        while count != 0:
+            count = 0
+            for i in range(len(bl)):
+                if i in disappear:
+                    continue
+                not_intersect = True
+                for j in range(i+1, len(bl)):
+                    if self.intersection_over_union(bl[i], bl[j]) > 0 and abs(bl[i].block.x_1-bl[j].block.x_1) < threshold:
+                        unions.append(bl[i].union(bl[j]))
+                        disappear.append(j)
+                        not_intersect = False
+                        count += 1
+                if not_intersect:
+                    unions.append(bl[i])
+            bl = unions
+            unions = []
+            disappear = []
+
+        return lay.Layout(bl)
+
+
+    def sorting(self, threshold=500):
+        bl = self.blocks
+        arr_im = np.asarray(self.image)
+
+        height, width = arr_im.shape[:2]
+
+        bl.sort(key = lambda b:b.coordinates[0], inplace=True)
+
+        intervals = []
+
+        for i in range(len(bl)):
+            x_1 = bl[i].block.x_1
+            if i == 0:
+                intervals.append(x_1)
+                curr_x = x_1
+            else:
+                if (x_1 - curr_x) >= threshold:
+                    intervals.append(x_1)
+                    curr_x = x_1  
+
+        blocks_arr = []
+
+        for i in range(len(intervals)):
+            if i == len(intervals) - 1:
+                curr_interval = le.Interval(intervals[i],width, axis='x').put_on_canvas(self.image)
+            else:
+                curr_interval = le.Interval(intervals[i], intervals[i+1], axis='x').put_on_canvas(self.image)
+            block = bl.filter_by(curr_interval, center=True)
+            block.sort(key = lambda b:b.coordinates[1], inplace=True)
+            blocks_arr.append(block)
+
+        bl = blocks_arr[0]
+        for i in range(1,len(blocks_arr)):
+            bl += blocks_arr[i]
+
+        return lay.Layout([b.set(id = idx) for idx, b in enumerate(bl)])
+
+
+    def left_to_right_overlap(self):
+        bl = self.blocks
+        for i in range(len(bl)):
+            for j in range(i+1, len(bl)):
+                if self.intersection_over_union(bl[i], bl[j]) > 0:
+                    right_rec_percentage = (bl[i].block.x_2-bl[j].block.x_1) / (bl[i].block.x_2-bl[i].block.x_1)
+                    left_rec_percentage  = (bl[i].block.x_2-bl[j].block.x_1) / (bl[j].block.x_2-bl[j].block.x_1)
+                    if right_rec_percentage >= left_rec_percentage:
+                        bl[j].block.x_1 = bl[i].block.x_2
+                    else:
+                        bl[i].block.x_2 = bl[j].block.x_1
+        
+        return lay.Layout(bl)
+
+
+    def pipeline(self, thres):
+        
+        if len(self.blocks) == 0:
+            raise Exception("NO TEXT BLOCKS")
+        
+        self.blocks = lay.Layout([i.pad(left=15, right=15, top=50, bottom=50) for i in self.blocks])
+    
+        self.blocks = self.unions(thres)
+
+        self.blocks = self.sorting(thres)
+
+        self.blocks = self.left_to_right_overlap()
+
+        return self.blocks
+
+
+
+
+
+
